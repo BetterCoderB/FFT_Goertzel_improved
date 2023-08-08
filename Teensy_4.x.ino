1e489bc6e67cabf77dbfcf93174755b171b0ec2e
BetterCoderB <107925928+BetterCoderB@users.noreply.github.com>
2023-08-08 15:36:14 +0300

    Add files via upload

---
+++ Teensy_4.x.ino
@@ -0,0 +1,48 @@
+#include <Arduino.h>
+#include <ADC.h>
+#include <ADC_util.h>
+ADC* adc = new ADC();
+
+const int DATA_SIZE = 256;
+const int frequency_k[] = { 30, 31, 32 };
+/* 
+5120Hz / 256 * 30 = 600 Hz
+            ...31 = 620 Hz
+            ...32 = 640 Hz
+*/
+const int quantity_k = sizeof(frequency_k) / sizeof(int);
+
+#include <FFT_Goertzel_improved.h>
+Goertzel<DATA_SIZE,(int*)frequency_k, quantity_k> FFT_Goertzel;
+
+
+void setup() {
+  Serial.begin(115200);
+  adc->adc0->setResolution(12);
+  adc->adc0->setReference(ADC_REFERENCE::REF_3V3);
+}
+void loop() {
+  long timer;
+  int value;
+  int max_level;
+  int i;
+  while (1) {
+    timer = micros(); //timer sampling timer
+
+    value = analogRead(A8) - 1560; //shift relative to zero
+    
+    FFT_Goertzel.write(value);        //write down the value
+    max_level = FFT_Goertzel.getmax();//get the maximum value in the given sample
+
+    for (i = 0; i < quantity_k; i++) {
+      value = (FFT_Goertzel.read(i) / max_level * 99); //normalize received value
+      Serial.print((int)value);
+      Serial.print(' ');
+    }
+    Serial.println();
+
+    while (micros() - timer < 195) //latency for fine sampling - 5120Hz this: 195.312us
+      ;
+    delayNanoseconds(312); //Teency 4.x
+  };
+}
